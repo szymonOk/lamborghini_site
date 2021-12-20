4e1eb3daadd3e77ce283ff2cc3734a1530f0e7cf
Szymon Okulowski <szymon.okulowski@gmail.com>
2021-12-20 18:58:54 +0100

    finish config section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 const hamBtn = document.querySelector('.hamburger')
 const mobileNav = document.querySelector('.nav-mobile')
+const navLink = document.querySelectorAll('.nav__link')
 const footerYear = document.querySelector('.footer__year')
 
 // ASIDE NAV
 const handleNav = () => {
 	hamBtn.classList.toggle('is-active')
 	mobileNav.classList.toggle('nav-mobile--active')
+
+	navLink.forEach(item => {
+		item.addEventListener('click', () => {
+			hamBtn.classList.remove('is-active')
+			mobileNav.classList.remove('nav-mobile--active')
+		})
+	})
 }
 
 // YEAR
